6995fd939046bc09fa8f7999c98de1a859d3ccaa
bsbds <69835502+bsbds@users.noreply.github.com>
2023-07-07 18:39:14 +0800

    docs: add example for kv client
    Signed-off-by: bsbds <69835502+bsbds@users.noreply.github.com>

---
+++ xline-client/examples/kv.rs
@@ -0,0 +1,72 @@
+use xline_client::{
+    error::ClientError as Error,
+    types::kv::{Compare, DeleteRangeRequest, PutRequest, RangeRequest, TxnOp, TxnRequest},
+    Client, ClientOptions,
+};
+use xlineapi::CompareResult;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    // the name and address of all curp members
+    let curp_members = [
+        ("server0", "10.0.0.1:2379"),
+        ("server1", "10.0.0.2:2379"),
+        ("server2", "10.0.0.3:2379"),
+    ]
+    .map(|(s, a)| (s.to_owned(), a.to_owned()));
+
+    let mut client = Client::connect(curp_members.into(), ClientOptions::default())
+        .await?
+        .kv_client();
+
+    // put
+    client.put(PutRequest::new("key1", "value1")).await?;
+    client.put(PutRequest::new("key2", "value2")).await?;
+
+    // range
+    let resp = client.range(RangeRequest::new("key1")).await?;
+
+    if let Some(kv) = resp.kvs.first() {
+        println!(
+            "got key: {}, value: {}",
+            String::from_utf8_lossy(&kv.key),
+            String::from_utf8_lossy(&kv.value)
+        );
+    }
+
+    // delete
+    let resp = client
+        .delete(DeleteRangeRequest::new("key1").with_prev_kv(true))
+        .await?;
+
+    for kv in resp.prev_kvs {
+        println!(
+            "deleted key: {}, value: {}",
+            String::from_utf8_lossy(&kv.key),
+            String::from_utf8_lossy(&kv.value)
+        );
+    }
+
+    // txn
+    let txn_req = TxnRequest::new()
+        .when(&[Compare::value("key2", CompareResult::Equal, "value2")][..])
+        .and_then(
+            &[TxnOp::put(
+                PutRequest::new("key2", "value3").with_prev_kv(true),
+            )][..],
+        )
+        .or_else(&[TxnOp::range(RangeRequest::new("key2"))][..]);
+
+    let _resp = client.txn(txn_req).await?;
+    let resp = client.range(RangeRequest::new("key2")).await?;
+    // should print "value3"
+    if let Some(kv) = resp.kvs.first() {
+        println!(
+            "got key: {}, value: {}",
+            String::from_utf8_lossy(&kv.key),
+            String::from_utf8_lossy(&kv.value)
+        );
+    }
+
+    Ok(())
+}
