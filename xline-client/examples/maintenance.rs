8f6a8f474229e8ea90030851e92eab1d26e1525e
bsbds <69835502+bsbds@users.noreply.github.com>
2023-07-07 18:57:40 +0800

    docs: add example for maintenance client
    Signed-off-by: bsbds <69835502+bsbds@users.noreply.github.com>

---
+++ xline-client/examples/maintenance.rs
@@ -0,0 +1,31 @@
+use xline_client::{error::ClientError as Error, Client, ClientOptions};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    // the name and address of all curp members
+    let curp_members = [
+        ("server0", "10.0.0.1:2379"),
+        ("server1", "10.0.0.2:2379"),
+        ("server2", "10.0.0.3:2379"),
+    ]
+    .map(|(s, a)| (s.to_owned(), a.to_owned()));
+
+    let mut client = Client::connect(curp_members.into(), ClientOptions::default())
+        .await?
+        .maintenance_client();
+
+    // snapshot
+    let mut msg = client.snapshot().await?;
+    let mut snapshot = vec![];
+    loop {
+        if let Some(resp) = msg.message().await? {
+            snapshot.extend_from_slice(&resp.blob);
+            if resp.remaining_bytes == 0 {
+                break;
+            }
+        }
+    }
+    println!("snapshot size: {}", snapshot.len());
+
+    Ok(())
+}
