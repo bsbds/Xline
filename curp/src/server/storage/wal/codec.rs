1c3f157d296dc70dd857a67e23f1f51748164eaf
bsbds <69835502+bsbds@users.noreply.github.com>
2023-11-24 21:37:54 +0800

    feat: impl codec

--- curp/src/server/storage/wal/codec.rs
+++ curp/src/server/storage/wal/codec.rs
@@ -1,6 +1,5 @@
 use std::{io, marker::PhantomData};
 
-use bytes::Buf;
 use curp_external_api::LogIndex;
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
@@ -8,83 +7,166 @@ use tokio_util::codec::{Decoder, Encoder};
 
 use crate::log_entry::LogEntry;
 
+trait FrameType {
+    fn frame_type(&self) -> u8;
+}
+
+trait FrameEncoder {
+    fn encode(&self) -> Vec<u8>;
+}
+
 #[derive(Debug)]
 enum WALFrame<C> {
-    /// Invalid Frame
-    Invalid,
-    /// A Frame containing a log entry
-    Entry(LogEntry<C>),
-    /// A Frame containing the sealed index
-    SealIndex(LogIndex),
-    /// A Frame containing the checksum
-    Commit(Vec<u8>),
+    Data(DataFrame<C>),
+    Commit(CommitFrame),
 }
 
 impl<C> WALFrame<C>
 where
-    C: Serialize,
+    C: for<'a> Deserialize<'a>,
 {
-    fn frame_type(&self) -> u8 {
-        match *self {
-            WALFrame::Invalid => 0x00,
-            WALFrame::Entry(_) => 0x01,
-            WALFrame::SealIndex(_) => 0x02,
-            WALFrame::Commit(_) => 0x03,
+    #[allow(clippy::indexing_slicing)] // The indexing is safe
+    fn decode(src: &[u8]) -> io::Result<Option<(Self, usize)>> {
+        if src.len() < 8 {
+            return Ok(None);
         }
+        let header: [u8; 8] = src[0..8]
+            .try_into()
+            .unwrap_or_else(|_| unreachable!("this conversion will always succeed"));
+        let frame_type = header[0];
+        Ok(Some(match frame_type {
+            0x01 => {
+                let len = Self::get_u64(header) as usize;
+                if src.len() < 8 + len {
+                    return Ok(None);
+                }
+                let payload = &src[8..8 + len];
+                let entry: LogEntry<C> = bincode::deserialize(payload)
+                    .map_err(|_| io::Error::from(io::ErrorKind::InvalidData))?;
+                (Self::Data(DataFrame::Entry(entry)), 8 + len)
+            }
+            0x02 => {
+                let index = Self::get_u64(header);
+                (Self::Data(DataFrame::SealIndex(index)), 8)
+            }
+            0x03 => {
+                if src.len() < 8 + 32 {
+                    return Ok(None);
+                }
+                let checksum = src[8..8 + 32].to_vec();
+                (Self::Commit(CommitFrame { checksum }), 8 + 32)
+            }
+            _ => {
+                return Err(io::Error::from(io::ErrorKind::Unsupported));
+            }
+        }))
     }
 
-    fn encode_frame(&self) -> Vec<u8> {
+    fn get_u64(mut header: [u8; 8]) -> u64 {
+        header.rotate_left(1);
+        header[7] = 0;
+        u64::from_le_bytes(header)
+    }
+}
+
+#[derive(Debug)]
+pub(crate) enum DataFrame<C> {
+    /// A Frame containing a log entry
+    Entry(LogEntry<C>),
+    /// A Frame containing the sealed index
+    SealIndex(LogIndex),
+}
+
+impl<C> FrameType for DataFrame<C> {
+    fn frame_type(&self) -> u8 {
         match *self {
-            WALFrame::Invalid => unreachable!("trying to encode an invalid frame"),
-            WALFrame::Entry(ref entry) => {
+            DataFrame::Entry(_) => 0x01,
+            DataFrame::SealIndex(_) => 0x02,
+        }
+    }
+}
+
+impl<C> FrameEncoder for DataFrame<C>
+where
+    C: Serialize,
+{
+    fn encode(&self) -> Vec<u8> {
+        match *self {
+            DataFrame::Entry(ref entry) => {
                 let entry_bytes = bincode::serialize(entry)
                     .unwrap_or_else(|_| unreachable!("serialization should never fail"));
                 let len = entry_bytes.len();
                 assert_eq!(len >> 56, 0, "log entry length: {len} too large");
-                let len_bytes = len.to_le_bytes().take(7);
+                let len_bytes = len.to_le_bytes().into_iter().take(7);
                 let header = [self.frame_type()].into_iter().chain(len_bytes);
                 header.chain(entry_bytes).collect()
             }
-            WALFrame::SealIndex(index) => {
+            DataFrame::SealIndex(index) => {
                 assert_eq!(index >> 56, 0, "log index: {index} too large");
                 // use the first 7 bytes
                 let index_bytes = index.to_le_bytes().into_iter().take(7);
                 [self.frame_type()].into_iter().chain(index_bytes).collect()
             }
-            WALFrame::Commit(checksum) => {
-                let header = [self.frame_type()].chain(&vec![0u8; 7]);
-
-                header.chain(checksum).collect()
-            }
         }
+    }
+}
+
+#[derive(Debug)]
+struct CommitFrame {
+    checksum: Vec<u8>,
+}
+
+impl CommitFrame {
+    fn new_from_data(data: &[u8]) -> Self {
+        Self {
+            checksum: Self::get_checksum(data),
+        }
+    }
+
+    /// Get the checksum of the slice, we use Sha256 as the hash function
+    fn get_checksum(data: &[u8]) -> Vec<u8> {
+        let mut hasher = Sha256::new();
+        hasher.update(data);
+        hasher.finalize().into_iter().collect()
+    }
+
+    fn validate(&self, data: &[u8]) -> bool {
+        self.checksum == Self::get_checksum(data)
+    }
+}
+
+impl FrameType for CommitFrame {
+    fn frame_type(&self) -> u8 {
+        0x03
+    }
+}
+
+impl FrameEncoder for CommitFrame {
+    fn encode(&self) -> Vec<u8> {
+        let header = [self.frame_type()].into_iter().chain([0u8; 7].into_iter());
+        header.chain(self.checksum.to_owned()).collect()
     }
 }
 
 /// The WAL codec
 #[derive(Debug)]
-pub(super) struct WAL<C> {
+pub(crate) struct WAL<C> {
     _phantom: PhantomData<C>,
 }
 
-impl<C> Encoder<Vec<WALFrame<C>>> for WAL<C>
+impl<C, D> Encoder<D> for WAL<C>
 where
     C: Serialize,
+    D: Iterator<Item = DataFrame<C>>,
 {
     type Error = io::Error;
 
-    fn encode(
-        &mut self,
-        entries: Vec<WALFrame<C>>,
-        dst: &mut bytes::BytesMut,
-    ) -> Result<(), Self::Error> {
-        let log_bytes = bincode::serialize(&item)
-            .unwrap_or_else(|_| unreachable!("serialization should never fail"));
+    fn encode(&mut self, frames: D, dst: &mut bytes::BytesMut) -> Result<(), Self::Error> {
+        let frames_bytes: Vec<_> = frames.into_iter().map(|f| f.encode()).flatten().collect();
+        let commit_frame = CommitFrame::new_from_data(&frames_bytes);
 
-        let record_len = log_bytes.len() + 32;
-
-        dst.extend(record_len.to_le_bytes());
-        dst.extend(Self::get_checksum(&log_bytes));
-        dst.extend(log_bytes);
+        dst.extend(frames_bytes);
+        dst.extend(commit_frame.encode());
 
         Ok(())
     }
@@ -92,50 +174,45 @@ where
 
 impl<C> Decoder for WAL<C>
 where
-    C: for<'a> Deserialize<'a>,
+    C: Serialize + for<'a> Deserialize<'a>,
 {
-    type Item = LogEntry<C>;
+    type Item = Vec<DataFrame<C>>;
 
     type Error = io::Error;
 
     fn decode(&mut self, src: &mut bytes::BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        // length bytes
-        if src.remaining() < 8 {
-            return Ok(None);
+        let mut frames = vec![];
+        loop {
+            match WALFrame::<C>::decode(src)? {
+                Some((frame, len)) => {
+                    let _ignore = src.split_to(len);
+                    match frame {
+                        WALFrame::Data(data) => {
+                            frames.push(data);
+                        }
+                        WALFrame::Commit(commit) => {
+                            let frames_bytes: Vec<_> =
+                                frames.iter().map(|d| d.encode()).flatten().collect();
+                            if commit.validate(&frames_bytes) {
+                                return Ok(Some(frames));
+                            } else {
+                                return Err(io::Error::from(io::ErrorKind::InvalidData));
+                            }
+                        }
+                    }
+                }
+                None => return Ok(None),
+            }
         }
-        let record_len = src.get_u64_le() as usize;
-        if src.remaining() < record_len {
-            return Ok(None);
-        }
-        let mut data = src.split_to(record_len);
-        let checksum = data.split_to(32);
-
-        if !Self::validate(&data, &checksum) {
-            return Err(io::Error::from(io::ErrorKind::InvalidData));
-        }
-
-        let log_entry = bincode::deserialize(&data)
-            .unwrap_or_else(|_| unreachable!("deserialization should never fail"));
-
-        Ok(log_entry)
     }
 }
 
 impl<C> WAL<C> {
+    /// Creates a new WAL codec
     pub(super) fn new() -> Self {
         Self {
             _phantom: PhantomData,
         }
-    }
-
-    fn get_checksum(data: &[u8]) -> Vec<u8> {
-        let mut hasher = Sha256::new();
-        hasher.update(&data);
-        hasher.finalize().into_iter().collect()
-    }
-
-    fn validate(data: &[u8], checksum: &[u8]) -> bool {
-        &Self::get_checksum(data)[..] == checksum
     }
 
     /// Get encoded length and padding length
--- curp/src/server/storage/wal/mod.rs
+++ curp/src/server/storage/wal/mod.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 mod codec;
 
 use std::{
@@ -11,7 +12,7 @@ use serde::Serialize;
 use tokio::fs::File as TokioFile;
 use tokio_util::codec::Framed;
 
-use self::codec::LogCodec;
+use self::codec::WAL;
 
 const SEGMENT_SIZE_BYTES: u64 = 64 * 1024 * 1024;
 
@@ -31,6 +32,7 @@ impl LogStorage {
         let file = OpenOptions::new()
             .create(true)
             .append(true)
+            .read(true)
             .open(dir.join(Self::log_name(0, 0)))?;
         file.try_lock_exclusive()?;
 
@@ -38,11 +40,6 @@ impl LogStorage {
             dir,
             lfiles: vec![TokioFile::from_std(file)],
         })
-    }
-
-    async fn test_file<C: Serialize>(&mut self) {
-        let file = self.lfiles.pop().unwrap();
-        let io = Framed::new(file, LogCodec::<C>::new());
     }
 
     /// Pre-allocates a file
@@ -54,8 +51,48 @@ impl LogStorage {
         file.allocate(size_in_bytes)
     }
 
+    pub(crate) fn framed_io<C: Serialize>(&mut self) -> Framed<TokioFile, WAL<C>> {
+        let file = self.lfiles.pop().unwrap();
+        Framed::new(file, WAL::<C>::new())
+    }
+
     /// Gets the name of the log file
     fn log_name(seq: u64, log_index: u64) -> String {
         format!("{seq}-{log_index}.log")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use curp_external_api::cmd::ProposeId;
+    use curp_test_utils::test_cmd::TestCommand;
+    use futures::SinkExt;
+    use tokio_stream::StreamExt;
+
+    use crate::{
+        log_entry::{EntryData, LogEntry},
+        server::storage::wal::codec::DataFrame,
+    };
+
+    use super::*;
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn append_and_read_is_ok() -> io::Result<()> {
+        let mut storage = LogStorage::new("/tmp/wal").await.unwrap();
+        let mut io = storage.framed_io();
+        let frame = DataFrame::Entry(LogEntry::<TestCommand>::new(
+            1,
+            1,
+            EntryData::Empty(ProposeId(1, 2)),
+        ));
+        io.send(vec![frame].into_iter()).await.unwrap();
+        drop(io);
+        drop(storage);
+
+        let mut storage = LogStorage::new("/tmp/wal").await?;
+        let mut io = storage.framed_io::<TestCommand>();
+        let frame = io.next().await.unwrap().unwrap();
+        println!("{:?}", frame[0]);
+        Ok(())
     }
 }
