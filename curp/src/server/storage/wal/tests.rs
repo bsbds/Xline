d1dc60ad1214c34be42fb91d8e7a26602645974f
bsbds <69835502+bsbds@users.noreply.github.com>
2023-12-01 10:33:16 +0800

    test: add head truncation test

--- curp/src/server/storage/wal/tests.rs
+++ curp/src/server/storage/wal/tests.rs
@@ -1,16 +1,18 @@
+use bytes::BytesMut;
 use curp_external_api::cmd::ProposeId;
 use curp_test_utils::test_cmd::TestCommand;
 use tempfile::TempDir;
+use tokio_util::codec::Encoder;
 
 use crate::{
     log_entry::{EntryData, LogEntry},
-    server::storage::wal::codec::DataFrame,
+    server::storage::wal::{codec::DataFrame, util::get_file_paths_with_ext},
 };
 
 use super::*;
 
 #[tokio::test(flavor = "multi_thread")]
-async fn log_append_and_recovery_is_ok() -> io::Result<()> {
+async fn log_append_and_recovery_is_ok() {
     let wal_test_path = tempfile::tempdir().unwrap();
     let config = WALConfig::new(wal_test_path);
     let (mut storage, _logs) = FramedWALStorage::new_or_recover(config.clone())
@@ -19,7 +21,7 @@ async fn log_append_and_recovery_is_ok() -> io::Result<()> {
 
     let num_entries = 10;
 
-    let entries = (1..=10)
+    let entries = (1..=num_entries)
         .map(|index| LogEntry::<TestCommand>::new(index, 1, EntryData::Empty(ProposeId(1, 2))));
 
     for entry in entries.clone() {
@@ -35,24 +37,24 @@ async fn log_append_and_recovery_is_ok() -> io::Result<()> {
         .await
         .unwrap();
 
-    assert_eq!(logs.len(), num_entries, "failed to recover all logs");
+    assert_eq!(
+        logs.len(),
+        num_entries as usize,
+        "failed to recover all logs"
+    );
     assert!(
         logs.into_iter().zip(entries).all(|(x, y)| x == y),
         "log entries mismatched"
     );
-
-    Ok(())
 }
 
 #[tokio::test(flavor = "multi_thread")]
-async fn log_tail_truncation_is_ok() -> io::Result<()> {
+async fn log_tail_truncation_is_ok() {
     let wal_test_path = tempfile::tempdir().unwrap();
     let config = WALConfig::new(wal_test_path);
     let (mut storage, _logs) = FramedWALStorage::new_or_recover(config.clone())
         .await
         .unwrap();
-
-    let num_entries = 10;
 
     let entries = (1..=10)
         .map(|index| LogEntry::<TestCommand>::new(index, 1, EntryData::Empty(ProposeId(1, 2))));
@@ -80,6 +82,71 @@ async fn log_tail_truncation_is_ok() -> io::Result<()> {
     assert_eq!(logs.len(), 8, "failed to recover all logs");
 
     assert_eq!(*logs.last().unwrap(), next_entry);
+}
 
-    Ok(())
+#[tokio::test(flavor = "multi_thread")]
+async fn log_head_truncation_is_ok() {
+    let wal_test_path = tempfile::tempdir().unwrap();
+    let max_segment_size = 512;
+    let header_size = 32;
+
+    let num_wals = || {
+        get_file_paths_with_ext(&wal_test_path, ".wal")
+            .unwrap()
+            .len()
+    };
+
+    let sample_entry = LogEntry::<TestCommand>::new(1, 1, EntryData::Empty(ProposeId(1, 2)));
+    let mut wal_codec = WAL::<TestCommand>::new();
+    let mut buf = BytesMut::new();
+    wal_codec.encode(vec![DataFrame::Entry(sample_entry)], &mut buf);
+    let entry_size = buf.len();
+
+    println!("entry size: {}", entry_size);
+    let num_entries_per_segment = (max_segment_size - header_size + entry_size - 1) / entry_size;
+
+    let config = WALConfig::new(&wal_test_path).with_max_segment_size(max_segment_size as u64);
+    let (mut storage, _logs) = FramedWALStorage::new_or_recover(config.clone())
+        .await
+        .unwrap();
+
+    let mut frame_gen = FrameGenerator::new();
+    println!("num entries: {}", num_entries_per_segment);
+    for frame in frame_gen.take(num_entries_per_segment) {
+        println!("sent");
+        storage.send_sync(vec![frame]).await.unwrap();
+    }
+    println!("2");
+
+    assert_eq!(num_wals(), 1);
+    storage.truncate_head(num_entries_per_segment as u64).await;
+    assert_eq!(num_wals(), 1);
+
+    storage.send_sync(vec![frame_gen.next()]);
+    assert_eq!(num_wals(), 2);
+    storage.truncate_head(num_entries_per_segment as u64).await;
+    assert_eq!(num_wals(), 1);
+}
+
+#[derive(Clone)]
+struct FrameGenerator {
+    next_index: u64,
+}
+
+impl FrameGenerator {
+    fn new() -> Self {
+        Self { next_index: 1 }
+    }
+
+    fn next(&mut self) -> DataFrame<TestCommand> {
+        let entry =
+            LogEntry::<TestCommand>::new(self.next_index, 1, EntryData::Empty(ProposeId(1, 2)));
+        self.next_index += 1;
+
+        DataFrame::Entry(entry)
+    }
+
+    fn take(&mut self, num: usize) -> Vec<DataFrame<TestCommand>> {
+        (0..num).map(|_| self.next()).collect()
+    }
 }
